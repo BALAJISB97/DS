4f2ea18e236684f561748a65164d1352f6981b9f
BALAJI <balajisb147@gmail.com>
2020-09-07 22:24:45 +0530

    Explanation

--- TreeRelatedQns/MaximumSumInALevel.py
+++ TreeRelatedQns/MaximumSumInALevel.py
@@ -31,3 +31,15 @@ class Solution:
             if node.right:
                 q.append(node.right)
         return la
+'''
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level X such that the sum of all the values of nodes at level X is maximal.
+Input: [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+'''
